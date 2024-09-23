daf706d549e8b5bd10e4c636c8eaf7b778e64593
nehalskdev <programmingwithnehalsk1999@gmail.com>
2024-09-23 15:54:51 +0530

    learned chaining promises

--- script.js
+++ script.js
@@ -87,8 +87,19 @@ console.log(request);
 
 // more simplified way
 const getCountryData = function (country) {
+  // country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]))
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      if (!neighbour) return;
+
+      // country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 getCountryData('UAE');
